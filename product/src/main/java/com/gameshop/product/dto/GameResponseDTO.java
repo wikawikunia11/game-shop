ab4341df0e14b2458d90f62b056947cd70c5c313
wikawikunia11 <usiamal@gmail.com>
2026-05-09 23:10:30 +0200

    Add GameService with CRUD and add Constructors to GameRequestDTO and GameResponseDTO

--- product/src/main/java/com/gameshop/product/dto/GameResponseDTO.java
+++ product/src/main/java/com/gameshop/product/dto/GameResponseDTO.java
@@ -1,13 +1,17 @@
 package com.gameshop.product.dto;
 
 import com.gameshop.product.model.AgeRestriction;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameResponseDTO {
     private Long id;
     private String title;
