0ce8f38943d40dcf208d4492e27f0fe1e98f76e6
wikawikunia11 <usiamal@gmail.com>
2026-05-09 20:02:50 +0200

    Add Game, Genre and AgeRestriction models

---
+++ product/src/main/java/com/gameshop/product/model/Genre.java
@@ -0,0 +1,17 @@
+package com.gameshop.product.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
